b8b2ce0c1b292150a84ef13d29c86c605180688b
Salvatore Bonaccorso <tury202737@hotmail.it>
2018-10-15 22:47:59 +0200

    Create 3-Somma di due numeri.c

---
+++ 3-Somma di due numeri.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* Secondo programma */
+
+int main() {
+ 	 int numero1, numero2 , somma;
+ 	 
+ 	 printf("digita il primo numero:\n");
+ 	 scanf("%d",&numero1);
+ 	 
+ 	 printf("digita il secondo numero:\n");
+ 	 scanf("%d",&numero2);
+ 	 
+ 	 somma=numero1 + numero2;
+ 	 
+ 	 printf("La somma dei due numeri e' uguale a :%d\n",somma);
+	return 0;
+ }
